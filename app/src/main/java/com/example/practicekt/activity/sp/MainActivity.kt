f68aa51a6c9c5124ac44f22c32fa6bbf2b63859b
Sumit Panchal <panchalsumit187@gmail.com>
2024-11-17 12:30:38 +0530

    Bug Fixing

--- app/src/main/java/com/example/practicekt/activity/sp/MainActivity.kt
+++ app/src/main/java/com/example/practicekt/activity/sp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.practicekt.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator.REQUEST_CODE
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -41,8 +42,7 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("name", "Sumit")
             intent.putExtra("age", 21)
             intent.putExtra("float", 187.87f)
-            startActivity(intent)
-//            binding.txtName.text = "E2Logy"
+            startActivityForResult(intent, REQUEST_CODE) // Start activity for result
         }
 
         binding.txtName.setOnClickListener {
@@ -54,7 +54,14 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            val returnedData = data?.getStringExtra("return_key")
+            binding.txtName.text = returnedData // Set the returned data to TextView
+        }
     }
 
     override fun onStart() {
@@ -91,9 +98,5 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop Method called")
-
-
     }
-
-
 }
